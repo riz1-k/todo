a60dec2ed28cdf6788b63e43268f360035843f03
Rizwan <rizwan.k.off@gmail.com>
2021-07-30 17:46:09 +0530

    ready again

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/todo', require('./server/routes/api/todoRoute'));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
-app.listen(3001, () =>
+app.listen(process.env.PORT || 3001, () =>
   console.log(
     '------------------------NODE server started on PORT 3001------------------------'
   )
